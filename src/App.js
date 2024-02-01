279c95ad8eafc924799c1574ba466b4c4ea6459c
Shubhi Shinde <shubhi.empyrean15@gmail.com>
2024-02-01 20:07:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import {apiUrl,filterData} from "./data";
+import { toast } from "react-toastify";
+import {useState , useEffect } from "react";
+import Spinner from "./components/Spinner";
+
+const App = () => {
+
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
+
+  async function fetchData(){
+
+    //loader on kr denge jab tak data fetch nhi ho jaye tab tak
+    setLoading(true);
+
+    try{
+      
+      const response = await fetch(apiUrl);
+      const output = await response.json();
+      // here output contains all the data fetched from the api
+      setCourses(output.data);
+
+    }
+    catch(error){
+      toast.error('Something went wrong');
+    }
+
+    //data fetch ho chuka hai to loader band kr denge
+    setLoading(false);
+  }
+
+  useEffect( ()=>{
+    fetchData();
+  },[]);
+
+  return (
+    <div className="min-h-screen flex flex-col bg-bgDark2">
+      <div>
+        <Navbar />
+      </div>
+      <div className="bg-bgDark2 ">
+        <div>
+          <Filter 
+              filterData = {filterData}
+                category = {category}
+                setCategory = {setCategory}
+              />
+        </div>
+        <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
+          {
+           loading ? (<Spinner />) : (<Cards courses={courses} category={category}/>) 
+          }
+        </div> 
+
+      </div>
+      
+    </div>
+  );
+};
+
+export default App;
